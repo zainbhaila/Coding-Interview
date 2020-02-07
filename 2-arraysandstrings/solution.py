111f218615e065749e92e8a4bc80e608d7c5b667
zainbhaila <zainbhaila@gmail.com>
2020-02-07 16:25:10 -0500

    hw2 init

---
+++ 2-arraysandstrings/solution.py
@@ -0,0 +1,11 @@
+# Given an array of size n, where the values are between 1 and n inclusive and
+# each value may at most appear twice, return a List of all the duplicate
+# elements. All tests have duplicates occur in ascending order (i.e. a duplicate
+# 1 would occur before a duplicate 2, etc), so your output MUST have the
+# duplicates in ascending order!
+
+
+from typing import List
+
+def findDuplicates(nums: List[int]) -> List[int]:
+    # TODO
