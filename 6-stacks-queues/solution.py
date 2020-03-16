d8ccddee12f9c508997dc6df00e608d72a23e4ab
zainbhaila <zainbhaila@gmail.com>
2020-03-16 14:08:10 -0400

    base cases 6, efficiency

--- 6-stacks-queues/solution.py
+++ 6-stacks-queues/solution.py
@@ -22,12 +22,12 @@
 """
 Zain Bhaila
 03/15/2020
-The three function takes three strings, two numeric and one operand and uses
-the operand on the given values. O(1) time and space.
+The three function takes three values, two int and one operator string and uses
+the operator on the given values. O(1) time and space.
 My implementation uses a stack to keep track of integers. It loops through the
 given list of tokens. If it is an integer it gets added to the stack. Otherwise,
 I apply the three function to the top two values in the stack and the current
-operand. Add the new value to the top of the stack and proceed. At the end,
+operator. Add the new value to the top of the stack and proceed. At the end,
 there will only be one value in the stack and none in tokens. Return the value
 in the stack.
 
@@ -36,28 +36,28 @@ Space Complexity: O(n) - uses an additional List as a stack, up to the size of t
 """
 
 from typing import List
-import re
 
-def three(val1, val2, operand): # takes two numbers and an operand string and applies the operand
-    val1 = int(val1) # convert inputs to int
-    val2 = int(val2)
-
-    if operand == "+": # determine the operand and return correct value
+def three(val1, val2, operator): # takes two ints and an operator string and applies the operator
+    if operator == "+": # determine the operator and return correct value
         return val1 + val2
-    elif operand == "-":
+    elif operator == "-":
         return val2 - val1
-    elif operand == "*":
-        return val1*val2
-    elif operand == "/":
-        return val2//val1 # integer division
+    elif operator == "*":
+        return val1 * val2
+    elif operator == "/":
+        return val2 // val1 # integer division
 
 def evalRPN(tokens: List[str]) -> int:
+    if (len(tokens) == 0): # base case for empty list
+        return 0
+
     stack = []
-    while (len(tokens) >= 1): # loop until all tokens are consumed
+    while (len(tokens) > 0): # loop until all tokens are consumed
         current = tokens.pop(0)
-        if (re.search("-?[0-9]+", str(current)) != None): # if integer, add to stack
-            stack.append(current)
-        else: # if token is operand, replace top two values in stack with operated values
+        if current not in ("+", "-", "*", "/"): # if integer, add to stack
+            stack.append(int(current))
+        else: # if token is operator, replace top two values in stack with operated values
             replacement = three(stack.pop(), stack.pop(), current)
             stack.append(replacement)
+
     return stack.pop()
